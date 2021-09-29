0399d14f138414513d98186fdb5141b0d67dfa0d
s2010515 <s2010515@ed.ac.uk>
2021-09-29 14:46:03 +0000

    allow other df

--- src/main/parralelism_metamap.py
+++ src/main/parralelism_metamap.py
@@ -29,6 +29,8 @@ def annotation_func(df,
                     batch, 
                     batch_size, 
                     mm,
+                    column_name,
+                    unique_id,
                     composite_phrase,
                     filename,
                     file_format,
@@ -63,17 +65,17 @@ def annotation_func(df,
     idx = []
     occurence = []
     negation = []
-    for j in range(len(df['content_text'])):
+    for j in range(len(df[column_name])):
         now = datetime.now()
         current_time = now.strftime("%d/%m/%Y, %H:%M:%S")
         print(str(current_time) + str(' We are starting processing row number ') + str(j+1+int((batch-1)*batch_size)))
         print(str(current_time) + str(' We are at row ') + str(j+1) + str(' out of ') + str(len(df)) + str(' from proccess ') + bold.BEGIN + str(batch) + bold.END)
         print(str('Proccess ') + bold.BEGIN + str(batch) + bold.END + str(' has completed ') + bold.BEGIN + str(round((float(j)/float(len(df)))*100, 2)) + str('%') + bold.END)
-        print(str(current_time) + str(' Processing ') + str(df.index.values[j]))
-        if df['content_text'][j] != df['content_text'][j] or df['content_text'][j] == None or df['content_text'][j] == '' or df['content_text'][j][:4] == 'ABS:':
+        print(str(current_time) + str(' Processing ') + str(df.iloc[j][unique_id]))
+        if df[column_name][j] != df[column_name][j] or df[column_name][j] == None or df[column_name][j] == '' or df[column_name][j][:4] == 'ABS:':
             pass
         else:
-            term = removeNonAscii(df['content_text'][j])
+            term = removeNonAscii(df[column_name][j])
             term = [term]
             ids = list(range(len(term)))
             concepts, error = mm.extract_concepts(term, 
@@ -121,7 +123,7 @@ def annotation_func(df,
                     neg = (int(str(text).count('noun-1')) + int(str(text).count('adj-1')) + int(str(text).count('verb-1')) + int(str(text).count('adv-1')) + int(str(text).count('integer-1')) + int(str(text).count('numeral-1')) + int(str(text).count('prep-1')) + int(str(text).count('number-1')) + int(str(text).count('percentage-1')) + int(str(text).count('conj-1')) + int(str(text).count('ordinal-1')) + int(str(text).count('UNKNOWN-1')) + int(str(text).count('aux-1')) + int(str(text).count('fraction-1')))
                     negation.append(neg)
                     occurence.append(neg + (int(str(text).count('noun-0')) + int(str(text).count('adj-0')) + int(str(text).count('verb-0')) + int(str(text).count('adv-0')) + int(str(text).count('integer-0')) + int(str(text).count('numeral-0')) + int(str(text).count('prep-0')) + int(str(text).count('number-0')) + int(str(text).count('percentage-0')) + int(str(text).count('conj-0')) + int(str(text).count('ordinal-0')) + int(str(text).count('UNKNOWN-0')) + int(str(text).count('aux-0')) + int(str(text).count('fraction-0'))))
-                    idx.append(df.index.values[j])
+                    idx.append(df.iloc[j][unique_id])
 
         if (j % 100 == 0 and j != 0):
             now = datetime.now()
@@ -134,9 +136,9 @@ def annotation_func(df,
                 'negation': negation,
                 'cui': list_of_cuis,
                 'annotation': list_of_annotations,
-                'retrieved_df2_idx' : idx
+                f'{unique_id}' : idx
                 })
-            pickle.dump(annotated_df, open(f'retrieved_annotated_content_df2_{batch}.p', 'wb'))
+            pickle.dump(annotated_df, open(f'retrieved_annotated_{column_name}_df2_{batch}.p', 'wb'))
                 
     now = datetime.now()
     current_time = now.strftime("%d/%m/%Y, %H:%M:%S")
@@ -149,13 +151,15 @@ def annotation_func(df,
         'negation': negation,
         'cui': list_of_cuis,
         'annotation': list_of_annotations,
-        'retrieved_df2_idx' : idx
+        f'{unique_id}' : idx
         })
-    pickle.dump(annotated_df, open(f'retrieved_annotated_content_df2_{batch}.p', 'wb'))
+    pickle.dump(annotated_df, open(f'retrieved_annotated_{column_name}_df2_{batch}.p', 'wb'))
                       
 
 def parralelism_metamap(numbers_of_cores,
                         path_to_metamap,
+                        column_name = 'content_text',
+                        unique_id = 'pmid',
                         path_to_file = None,
                         file = None,
                         composite_phrase=4,
@@ -206,18 +210,18 @@ def parralelism_metamap(numbers_of_cores,
     
     pmids = []
     for i in range(len(df)):
-        if df['content_text'][i] != df['content_text'][i] or df['content_text'][i] == None or df['content_text'][i] == '' or df['content_text'][i][:4] == 'ABS:':
+        if df[column_name][i] != df[column_name][i] or df[column_name][i] == None or df[column_name][i] == '' or df[column_name][i][:4] == 'ABS:':
             pass
         else:
-            if len(df.iloc[i].content_text.split()) > 150000:
+            if len(df.iloc[i][column_name].split()) > 150000:
                 pass
             else:
-                pmids.append(df.iloc[i].pmid)
-    df = df[df.pmid.isin(pmids)]
+                pmids.append(df.iloc[i][unique_id])
+    df = df[df[unique_id].isin(pmids)]
 
     update = False
 
-    retrieved_path = [path for path in Path('./').iterdir() if path.stem == 'retrieved_annotated_content_df2']
+    retrieved_path = [path for path in Path('./').iterdir() if path.stem == f'retrieved_annotated_{column_name}_df2']
     if len(retrieved_path) == 0:
         update = False
     elif retrieved_path[0]:
@@ -228,51 +232,49 @@ def parralelism_metamap(numbers_of_cores,
 
     if update == True:
 
-        df_processed = pickle.load(open('retrieved_annotated_content_df2.p', 'rb'))
+        df_processed = pickle.load(open(f'retrieved_annotated_{column_name}_df2.p', 'rb'))
 
         list_original_index = []
         for i in range(len(df)):
-            list_original_index.append(df.index[i])
+            list_original_index.append(df.iloc[i][unique_id])
 
         list_check_index = []
-        list_check_index = list(np.unique(df_processed.retrieved_df2_idx))
+        list_check_index = list(np.unique(df_processed[unique_id]))
 
         list_to_do = list(set(list_original_index) - set(list_check_index))
 
-        subset_list_update = []
-        for i in range(len(list_to_do)):
-            subset_list_update.append(df.loc[list_to_do[i]].pmid)
+        subset_list_update = list_to_do
 
-        df = df[df.pmid.isin(subset_list_update)]
+        df = df[df[unique_id].isin(subset_list_update)]
 
     mm = MetaMap.get_instance(path_to_metamap)
 
-    df_size = len(df['content_text'])
+    df_size = len(df[column_name])
     batch_size = round(len(df)/par_core)
 
     if par_core > 1:
         data = []
         for i in range(par_core):
             if i == 0:
-                current_data = (df[:round(len(df)/par_core)], i+1, batch_size, mm, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
+                current_data = (df[:round(len(df)/par_core)], i+1, batch_size, mm, column_name, unique_id, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
                         allow_large_n, strict_model, relaxed_model, allow_overmatches, allow_concept_gaps, term_processing, no_derivational_variants,
                         derivational_variants, ignore_word_order, unique_acronym_variants, prefer_multiple_concepts, ignore_stop_phrases, compute_all_mappings,
                         prune, mm_data_version, verbose, exclude_sources, restrict_to_sources, restrict_to_sts, exclude_sts, no_nums)
                 data.append(current_data)
             elif i > 0 and i+1 != par_core:
-                current_data = (df[(round(len(df)/par_core))*i:(round(len(df)/par_core))*(i+1)], i+1, batch_size, mm, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
+                current_data = (df[(round(len(df)/par_core))*i:(round(len(df)/par_core))*(i+1)], i+1, batch_size, mm, column_name, unique_id, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
                         allow_large_n, strict_model, relaxed_model, allow_overmatches, allow_concept_gaps, term_processing, no_derivational_variants,
                         derivational_variants, ignore_word_order, unique_acronym_variants, prefer_multiple_concepts, ignore_stop_phrases, compute_all_mappings,
                         prune, mm_data_version, verbose, exclude_sources, restrict_to_sources, restrict_to_sts, exclude_sts, no_nums)
                 data.append(current_data)
             else:
-                current_data = (df[round(len(df)/par_core)*i:], i+1, batch_size, mm, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
+                current_data = (df[round(len(df)/par_core)*i:], i+1, batch_size, mm, column_name, unique_id, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
                         allow_large_n, strict_model, relaxed_model, allow_overmatches, allow_concept_gaps, term_processing, no_derivational_variants,
                         derivational_variants, ignore_word_order, unique_acronym_variants, prefer_multiple_concepts, ignore_stop_phrases, compute_all_mappings,
                         prune, mm_data_version, verbose, exclude_sources, restrict_to_sources, restrict_to_sts, exclude_sts, no_nums)
                 data.append(current_data)
     else:
-        data = [(df, 1, batch_size, mm, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
+        data = [(df, 1, batch_size, mm, column_name, unique_id, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
                         allow_large_n, strict_model, relaxed_model, allow_overmatches, allow_concept_gaps, term_processing, no_derivational_variants,
                         derivational_variants, ignore_word_order, unique_acronym_variants, prefer_multiple_concepts, ignore_stop_phrases, compute_all_mappings,
                         prune, mm_data_version, verbose, exclude_sources, restrict_to_sources, restrict_to_sts, exclude_sts, no_nums)]
@@ -280,21 +282,21 @@ def parralelism_metamap(numbers_of_cores,
     with mp.Pool(numbers_of_cores) as pool:
         pool.starmap(annotation_func, data)
     
-    concat_df = pickle.load(open(f'retrieved_annotated_content_df2_1.p', 'rb'))
+    concat_df = pickle.load(open(f'retrieved_annotated_{column_name}_df2_1.p', 'rb'))
     if numbers_of_cores > 1:
         for i in range(par_core):
-            df_dynamic = pickle.load(open(f'retrieved_annotated_content_df2_{i+1}.p', 'rb'))
+            df_dynamic = pickle.load(open(f'retrieved_annotated_{column_name}_df2_{i+1}.p', 'rb'))
             concat_df = pd.concat([concat_df, df_dynamic])
 
 
-    pickle.dump(concat_df, open(f'retrieved_annotated_content_df.p', 'wb'))
+    pickle.dump(concat_df, open(f'retrieved_annotated_{column_name}_df.p', 'wb'))
     
     if update == True:
         final_df = pd.concat([df_processed, concat_df])
     else:
         final_df = concat_df
 
-    pickle.dump(final_df, open(f'retrieved_annotated_content_df2.p', 'wb'))
+    pickle.dump(final_df, open(f'retrieved_annotated_{column_name}_df2.p', 'wb'))
 
 
     now = datetime.now()
