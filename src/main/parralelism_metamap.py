391307a5ee5fe557135830214eee41909c890a38
s2010515 <s2010515@ed.ac.uk>
2021-10-04 14:40:46 +0000

    creation of the output structure

--- src/main/parralelism_metamap.py
+++ src/main/parralelism_metamap.py
@@ -16,6 +16,21 @@ class bold:
    BEGIN = '\033[1m'
    END = '\033[0m'
 
+def output_files(column_name, extension):
+    #creating the directories we are planning on using the save the result of the system
+    for path in [f'output_ParallelPyMetaMap_{column_name}',
+                f'output_ParallelPyMetaMap_{column_name}/annotated_df',
+                f'output_ParallelPyMetaMap_{column_name}/temporary_df',
+                f'output_ParallelPyMetaMap_{column_name}/{extension}_files'
+                ]:
+        try:
+            #try to create the directory, most likely will work for new project
+            os.mkdir(path)
+            print(f'Now creating {path}')
+        except:
+            #if the directory already exist just pass
+            pass
+
 def removeNonAscii(s):
     return "".join(filter(lambda x: ord(x)<128, s))
 
@@ -31,6 +46,8 @@ def annotation_func(df,
                     mm,
                     column_name,
                     unique_id,
+                    extension,
+                    extension_format,
                     composite_phrase,
                     filename,
                     file_format,
@@ -124,6 +141,14 @@ def annotation_func(df,
                     negation.append(neg)
                     occurence.append(neg + (int(str(text).count('noun-0')) + int(str(text).count('adj-0')) + int(str(text).count('verb-0')) + int(str(text).count('adv-0')) + int(str(text).count('integer-0')) + int(str(text).count('numeral-0')) + int(str(text).count('prep-0')) + int(str(text).count('number-0')) + int(str(text).count('percentage-0')) + int(str(text).count('conj-0')) + int(str(text).count('ordinal-0')) + int(str(text).count('UNKNOWN-0')) + int(str(text).count('aux-0')) + int(str(text).count('fraction-0'))))
                     idx.append(df.iloc[j][unique_id])
+                    if extension_format == 'dict':
+                        f = open(f"./output_ParallelPyMetaMap_{column_name}/{extension}_files/{df.iloc[j][unique_id]}.{extension}", "a")
+                        f.write(str(str(data[str(0)][i]) + ('\n')))
+                        f.close()
+                    elif extension_format == 'terminal':
+                        f = open(f"./output_ParallelPyMetaMap_{column_name}/{extension}_files/{df.iloc[j][unique_id]}.{extension}", "a")
+                        f.write(str('USER|') + str(data[str(0)][i].get('mm')) + str('|') + str(data[str(0)][i].get('score')) + str('|') + str(data[str(0)][i].get('preferred_name')) + str('|') + str(data[str(0)][i].get('cui')) + str('|') + str(data[str(0)][i].get('semtypes')) + str('|') + str(data[str(0)][i].get('trigger')) + str('|') + str(data[str(0)][i].get('location')) + str('|') + str(data[str(0)][i].get('pos_info')) + str('|') + str(data[str(0)][i].get('tree_codes')) + str(('\n'))) 
+                        f.close()
 
         if (j % 100 == 0 and j != 0):
             now = datetime.now()
@@ -138,7 +163,7 @@ def annotation_func(df,
                 'annotation': list_of_annotations,
                 f'{unique_id}' : idx
                 })
-            pickle.dump(annotated_df, open(f'retrieved_annotated_{column_name}_df2_{batch}.p', 'wb'))
+            pickle.dump(annotated_df, open(f'./output_ParallelPyMetaMap_{column_name}/temporary_df/annotated_{column_name}_df2_{batch}.p', 'wb'))
                 
     now = datetime.now()
     current_time = now.strftime("%d/%m/%Y, %H:%M:%S")
@@ -153,13 +178,15 @@ def annotation_func(df,
         'annotation': list_of_annotations,
         f'{unique_id}' : idx
         })
-    pickle.dump(annotated_df, open(f'retrieved_annotated_{column_name}_df2_{batch}.p', 'wb'))
+    pickle.dump(annotated_df, open(f'./output_ParallelPyMetaMap_{column_name}/temporary_df/annotated_{column_name}_df2_{batch}.p', 'wb'))
                       
 
 def parralelism_metamap(numbers_of_cores,
                         path_to_metamap,
                         column_name = 'content_text',
                         unique_id = 'pmid',
+                        extension = 'txt',
+                        extension_format = 'terminal',
                         path_to_file = None,
                         file = None,
                         composite_phrase=4,
@@ -189,6 +216,8 @@ def parralelism_metamap(numbers_of_cores,
                         exclude_sts=[],
                         no_nums=[]):
 
+    output_files(column_name, extension)
+
     if numbers_of_cores >= mp.cpu_count():
         return 'The number of cores you want to use is equal or greater than the numbers of cores in your machine. We stop the script now'
         exit()
@@ -208,6 +237,9 @@ def parralelism_metamap(numbers_of_cores,
         return 'You did not input any data to process'
         exit()
     
+    if len(df) < par_core:
+            par_core = len(df)
+    
     pmids = []
     for i in range(len(df)):
         if df[column_name][i] != df[column_name][i] or df[column_name][i] == None or df[column_name][i] == '' or df[column_name][i][:4] == 'ABS:':
@@ -217,11 +249,24 @@ def parralelism_metamap(numbers_of_cores,
                 pass
             else:
                 pmids.append(df.iloc[i][unique_id])
-    df = df[df[unique_id].isin(pmids)]
+        if df.iloc[i][unique_id] != df.iloc[i][unique_id] or df.iloc[i][unique_id] == None or df.iloc[i][unique_id] == '':
+            print('Your unique identifier is empty/None/NaN, please choose a unique identifier present for each row.')
+            return None
+            exit()
+        if '/' in str(df.iloc[i][unique_id]):
+            print('Your unique identifier contains "/" please choose another unique identifier, remove the "/" from the current unique identifier or replace "/" with another character.')
+            return None
+            exit()
+    if len(np.unique(pmids)) == len(pmids):
+        df = df[df[unique_id].isin(pmids)]
+    else:
+        print('It seems that one of your unique identifier is duplicate, please choose a unique identifier present for each row.')
+        return None
+        exit()
 
     update = False
 
-    retrieved_path = [path for path in Path('./').iterdir() if path.stem == f'retrieved_annotated_{column_name}_df2']
+    retrieved_path = [path for path in Path(f'output_ParallelPyMetaMap_{column_name}/annotated_df').iterdir() if path.stem == f'annotated_{column_name}_{unique_id}_df2']
     if len(retrieved_path) == 0:
         update = False
     elif retrieved_path[0]:
@@ -232,7 +277,7 @@ def parralelism_metamap(numbers_of_cores,
 
     if update == True:
 
-        df_processed = pickle.load(open(f'retrieved_annotated_{column_name}_df2.p', 'rb'))
+        df_processed = pickle.load(open(f'./output_ParallelPyMetaMap_{column_name}/annotated_df/annotated_{column_name}_{unique_id}_df2.p', 'rb'))
 
         list_original_index = []
         for i in range(len(df)):
@@ -246,6 +291,11 @@ def parralelism_metamap(numbers_of_cores,
         subset_list_update = list_to_do
 
         df = df[df[unique_id].isin(subset_list_update)]
+        if len(df) == 0:
+            print('No new row since last time.')
+            return None
+        if len(df) < par_core:
+            par_core = len(df)
 
     mm = MetaMap.get_instance(path_to_metamap)
 
@@ -256,25 +306,25 @@ def parralelism_metamap(numbers_of_cores,
         data = []
         for i in range(par_core):
             if i == 0:
-                current_data = (df[:round(len(df)/par_core)], i+1, batch_size, mm, column_name, unique_id, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
+                current_data = (df[:round(len(df)/par_core)], i+1, batch_size, mm, column_name, unique_id, extension, extension_format, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
                         allow_large_n, strict_model, relaxed_model, allow_overmatches, allow_concept_gaps, term_processing, no_derivational_variants,
                         derivational_variants, ignore_word_order, unique_acronym_variants, prefer_multiple_concepts, ignore_stop_phrases, compute_all_mappings,
                         prune, mm_data_version, verbose, exclude_sources, restrict_to_sources, restrict_to_sts, exclude_sts, no_nums)
                 data.append(current_data)
             elif i > 0 and i+1 != par_core:
-                current_data = (df[(round(len(df)/par_core))*i:(round(len(df)/par_core))*(i+1)], i+1, batch_size, mm, column_name, unique_id, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
+                current_data = (df[(round(len(df)/par_core))*i:(round(len(df)/par_core))*(i+1)], i+1, batch_size, mm, column_name, unique_id, extension, extension_format, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
                         allow_large_n, strict_model, relaxed_model, allow_overmatches, allow_concept_gaps, term_processing, no_derivational_variants,
                         derivational_variants, ignore_word_order, unique_acronym_variants, prefer_multiple_concepts, ignore_stop_phrases, compute_all_mappings,
                         prune, mm_data_version, verbose, exclude_sources, restrict_to_sources, restrict_to_sts, exclude_sts, no_nums)
                 data.append(current_data)
             else:
-                current_data = (df[round(len(df)/par_core)*i:], i+1, batch_size, mm, column_name, unique_id, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
+                current_data = (df[round(len(df)/par_core)*i:], i+1, batch_size, mm, column_name, unique_id, extension, extension_format, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
                         allow_large_n, strict_model, relaxed_model, allow_overmatches, allow_concept_gaps, term_processing, no_derivational_variants,
                         derivational_variants, ignore_word_order, unique_acronym_variants, prefer_multiple_concepts, ignore_stop_phrases, compute_all_mappings,
                         prune, mm_data_version, verbose, exclude_sources, restrict_to_sources, restrict_to_sts, exclude_sts, no_nums)
                 data.append(current_data)
     else:
-        data = [(df, 1, batch_size, mm, column_name, unique_id, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
+        data = [(df, 1, batch_size, mm, column_name, unique_id, extension, extension_format, composite_phrase, filename, file_format, allow_acronym_variants, word_sense_disambiguation,
                         allow_large_n, strict_model, relaxed_model, allow_overmatches, allow_concept_gaps, term_processing, no_derivational_variants,
                         derivational_variants, ignore_word_order, unique_acronym_variants, prefer_multiple_concepts, ignore_stop_phrases, compute_all_mappings,
                         prune, mm_data_version, verbose, exclude_sources, restrict_to_sources, restrict_to_sts, exclude_sts, no_nums)]
@@ -282,21 +332,21 @@ def parralelism_metamap(numbers_of_cores,
     with mp.Pool(numbers_of_cores) as pool:
         pool.starmap(annotation_func, data)
     
-    concat_df = pickle.load(open(f'retrieved_annotated_{column_name}_df2_1.p', 'rb'))
-    if numbers_of_cores > 1:
+    concat_df = pickle.load(open(f'./output_ParallelPyMetaMap_{column_name}/temporary_df/annotated_{column_name}_df2_1.p', 'rb'))
+    if par_core > 1:
         for i in range(par_core):
-            df_dynamic = pickle.load(open(f'retrieved_annotated_{column_name}_df2_{i+1}.p', 'rb'))
+            df_dynamic = pickle.load(open(f'./output_ParallelPyMetaMap_{column_name}/temporary_df/annotated_{column_name}_df2_{i+1}.p', 'rb'))
             concat_df = pd.concat([concat_df, df_dynamic])
 
 
-    pickle.dump(concat_df, open(f'retrieved_annotated_{column_name}_df.p', 'wb'))
+    pickle.dump(concat_df, open(f'./output_ParallelPyMetaMap_{column_name}/annotated_df/annotated_{column_name}_{unique_id}_df.p', 'wb'))
     
     if update == True:
         final_df = pd.concat([df_processed, concat_df])
     else:
         final_df = concat_df
 
-    pickle.dump(final_df, open(f'retrieved_annotated_{column_name}_df2.p', 'wb'))
+    pickle.dump(final_df, open(f'./output_ParallelPyMetaMap_{column_name}/annotated_df/annotated_{column_name}_{unique_id}_df2.p', 'wb'))
 
 
     now = datetime.now()
