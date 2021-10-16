f7293a303ef37deaad5159a32a190e7e152538af
s2010515 <s2010515@ed.ac.uk>
2021-10-16 15:19:50 +0000

    update setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['collections',
+    install_requires=[
 'abc',
 'os',
 'subprocess',
